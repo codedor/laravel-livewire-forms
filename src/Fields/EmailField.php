437ea20f377179a7cbbff9a3b6b5f7dc8f0d72cf
Sofian Mourabit <sofian@codedor.be>
2020-07-07 15:32:36 +0200

    Added Number / Email / Password fields

---
+++ src/Fields/EmailField.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Codedor\LivewireForms\Fields;
+
+class EmailField extends Field
+{
+    public $component = 'livewire-forms::fields.text';
+    public $type = 'email';
+}
---
+++ src/Fields/NumberField.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Codedor\LivewireForms\Fields;
+
+class NumberField extends Field
+{
+    public $component = 'livewire-forms::fields.text';
+    public $type = 'number';
+}
