575e4c94933fd8f90b9ecf8363498e655ade2625
Sander Van Damme <sander@codedor.be>
2020-07-07 09:23:05 +0200

    Initial commit

---
+++ src/LivewireFormsServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Codedor\LivewireForms;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Nova;
+
+class LivewireFormsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        die;
+        //
+    }
+}
