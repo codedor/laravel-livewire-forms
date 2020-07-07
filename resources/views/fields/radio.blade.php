1b4cd20d7bee966ddd11b0c93d2b729b8c471c72
Sofian Mourabit <sofian@codedor.be>
2020-07-07 14:56:54 +0200

    Finnish up radio group

--- resources/views/fields/radio.blade.php
+++ resources/views/fields/radio.blade.php
@@ -1,17 +1,18 @@
 <div class="{{ $field->divClass ?? 'col-6' }} form-group required">
      @foreach ($field->options as $value => $label)
     <label class="flex items-center select-none" for="{{ $field->name . '.' . $loop->index }}">
-    <input
-        type="radio"
-        class="{{ $field->class }}"
-        id="{{ $field->name . '.' . $loop->index }}"
-        name="{{ $field->name }}"
-        value="{{ $value }}"
-        wire:model.debounce.0ms="fields.{{ $field->name . '.' . $loop->index }}"
-    >
+        <input
+            type="radio"
+            class="{{ $field->class }}"
+            id="{{ $field->name . '.' . $loop->index }}"
+            name="{{ $field->name }}"
+            value="{{ $value }}"
+            wire:model="fields.{{ $field->name }}"
+        >
         {{ $label }}
     </label>
     @endforeach
+@dump($field->default)
 
     @include('livewire-forms::fields.error')
 </div>
