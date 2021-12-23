a208f694bd4f9f49acd53bde4e15291ed6749ccd
Jyrki De Neve <jyrki.deneve@gmail.com>
2021-12-23 13:22:09 +0100

    Remove tooltip.blade.php component

--- resources/views/components/tooltip.blade.php
+++
@@ -1,4 +0,0 @@
-<button class="tooltip" aria-label="Tooltip info label" type="button">
-    <i class="fal fa-info-circle gdpr-icon" aria-hidden="true"></i>
-    <span class="gdpr-info">{{ $text }}</span>
-</button>
